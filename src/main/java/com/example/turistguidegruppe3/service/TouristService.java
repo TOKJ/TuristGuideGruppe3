ffcd6c71043a45758eb6e026c26f8dad8f3860d3
Frederick <frdu0001@stud@ek.dk>
2026-01-28 10:28:59 +0100

    TouristService Added

--- src/main/java/com/example/turistguidegruppe3/service/TouristService.java
+++ src/main/java/com/example/turistguidegruppe3/service/TouristService.java
@@ -1,4 +1,23 @@
 package com.example.turistguidegruppe3.service;
 
+import com.example.turistguidegruppe3.model.TouristAttraction;
+import com.example.turistguidegruppe3.repository.TouristRepository;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+
+@Service
 public class TouristService {
+    private final TouristRepository repository;
+
+
+    public TouristService(TouristRepository repository) {
+        this.repository = repository;
+    }
+
+    public ArrayList<TouristAttraction> GetAttractions(){
+        return repository.getAllAttractions();
+    }
+
+
 }
