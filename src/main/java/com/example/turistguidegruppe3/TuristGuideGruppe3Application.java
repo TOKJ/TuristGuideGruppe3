2ef2cb1a2e587efb029e5a46fc10cf5cf5d75df1
TOKJ <tokj0001@stud.ek.dk>
2026-01-28 09:48:49 +0100

    Test

---
+++ src/main/java/com/example/turistguidegruppe3/TuristGuideGruppe3Application.java
@@ -0,0 +1,13 @@
+package com.example.turistguidegruppe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristGuideGruppe3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristGuideGruppe3Application.class, args);
+    }
+
+}
