1c4c942454400e19fab789bb7dc9c3b9469deb27
AlexArnold01 <alar1000@stud.ek.dk>
2026-01-28 11:00:34 +0100

    day 1 - startet på @PostMapping("/add") metoden

--- src/main/java/com/example/turistguidegruppe3/controller/TouristController.java
+++ src/main/java/com/example/turistguidegruppe3/controller/TouristController.java
@@ -5,10 +5,7 @@ import com.example.turistguidegruppe3.service.TouristService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -50,7 +47,10 @@ public class TouristController {
         }
     }
 
-
+    @PostMapping("/add")
+    public ResponseEntity<TouristAttraction> addMessage(@RequestBody TouristAttraction touristAttraction) {
+        if ()
+    }
     //TODO: post /attractions/add
 
 
